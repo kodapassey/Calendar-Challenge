88b3815a0498093bec9a40f03e2254f2879a23d0
kodapassey <kodapassey@charter.net>
2022-06-07 18:35:37 -0700

    time now turns red after time passes and will reset for new day where before I had to manually change the day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,25 @@
 // simple jquery method using moment.js to show time with #time id selected and .text
 $("#time").text(moment().format('ddd MMM Do, YYYY'));
 
-const eightAm = new Date('June 7, 2022 08:00:00');
-const nineAm = new Date('June 7, 2022 09:00:00');
-const tenAm = new Date('June 7, 2022 10:00:00');
-const elevenAm = new Date('June 7, 2022 11:00:00');
-const twelvePm = new Date('June 7, 2022 12:00:00');
-const onePm = new Date('June 7, 2022 13:00:00');
-const twoPm = new Date('June 7, 2022 14:00:00');
-const threePm = new Date('June 7, 2022 15:00:00');
-const fourPm = new Date('June 7, 2022 16:00:00');
-const fivePm = new Date('June 7, 2022 17:00:00');
+const eightAm = moment().set('hour', 8);
+const nineAm = moment().set('hour', 9);
+const tenAm = moment().set('hour', 10);
+const elevenAm = moment().set('hour', 11);
+const twelvePm = moment().set('hour', 12);
+const onePm = moment().set('hour', 13);
+const twoPm = moment().set('hour', 14);
+const threePm = moment().set('hour', 15);
+const fourPm = moment().set('hour', 16);
+const fivePm = moment().set('hour', 17);
+const sixPm = moment().set('hour', 18);
 
-// function to update background color every minute
-function updateBackground() {
+// function to check time and see if time has passed, functions interval is set to 1 minute
+function checkTime() {
 
     // 8 am section
     const eightAmEl = document.getElementById('eightAmEl');
     const eightAmElInput = document.getElementById('eightAmElInput');
-    if (eightAm < Date.now()) {
+    if (moment().isAfter(eightAm)) {
         eightAmEl.style.backgroundColor = 'red';
         eightAmElInput.style.backgroundColor = 'red';
         // added display none below so that once time passes, you can no longer input a task
@@ -31,7 +32,7 @@ function updateBackground() {
     //9 am section
     const nineAmEl = document.getElementById('nineAmEl');
     const nineAmElInput = document.getElementById('nineAmElInput');
-    if (nineAm < Date.now()) {
+    if (moment().isAfter(nineAm)) {
         nineAmEl.style.backgroundColor = 'red';
         nineAmElInput.style.backgroundColor = 'red';
         // added display none below so that once time passes, you can no longer input a task
@@ -45,7 +46,7 @@ function updateBackground() {
     //10 am section
     const tenAmEl = document.getElementById('tenAmEl');
     const tenAmElInput = document.getElementById('tenAmElInput');
-    if (tenAm < Date.now()) {
+    if (moment().isAfter(tenAm)) {
         tenAmEl.style.backgroundColor = 'red';
         tenAmElInput.style.backgroundColor = 'red';
         // added display none below so that once time passes, you can no longer input a task
@@ -59,7 +60,7 @@ function updateBackground() {
     //11 am section
     const elevenAmEl = document.getElementById('elevenAmEl');
     const elevenAmElInput = document.getElementById('elevenAmElInput');
-    if (elevenAm < Date.now()) {
+    if (moment().isAfter(elevenAm)) {
         elevenAmEl.style.backgroundColor = 'red';
         elevenAmElInput.style.backgroundColor = 'red';
         // added display none below so that once time passes, you can no longer input a task
@@ -73,7 +74,7 @@ function updateBackground() {
     //noon section
     const twelvePmEl = document.getElementById('twelvePmEl');
     const twelvePmElInput = document.getElementById('twelvePmElInput');
-    if (twelvePm < Date.now()) {
+    if (moment().isAfter(twelvePm)) {
         twelvePmEl.style.backgroundColor = 'red';
         twelvePmElInput.style.backgroundColor = 'red';
         // added display none below so that once time passes, you can no longer input a task
@@ -87,7 +88,7 @@ function updateBackground() {
     //1 pm section
     const onePmEl = document.getElementById('onePmEl');
     const onePmElInput = document.getElementById('onePmElInput');
-    if (onePm < Date.now()) {
+    if (moment().isAfter(onePm)) {
         onePmEl.style.backgroundColor = 'red';
         onePmElInput.style.backgroundColor = 'red';
         // added display none below so that once time passes, you can no longer input a task
@@ -101,7 +102,7 @@ function updateBackground() {
     //2 pm section
     const twoPmEl = document.getElementById('twoPmEl');
     const twoPmElInput = document.getElementById('twoPmElInput');
-    if (twoPm < Date.now()) {
+    if (moment().isAfter(twoPm)) {
         twoPmEl.style.backgroundColor = 'red';
         twoPmElInput.style.backgroundColor = 'red';
         // added display none below so that once time passes, you can no longer input a task
@@ -115,7 +116,7 @@ function updateBackground() {
     //3pm section
     const threePmEl = document.getElementById('threePmEl');
     const threePmElInput = document.getElementById('threePmElInput');
-    if (threePm < Date.now()) {
+    if (moment().isAfter(threePm)) {
         threePmEl.style.backgroundColor = 'red';
         threePmElInput.style.backgroundColor = 'red';
         // added display none below so that once time passes, you can no longer input a task
@@ -129,7 +130,7 @@ function updateBackground() {
     //4 pm section
     const fourPmEl = document.getElementById('fourPmEl');
     const fourPmElInput = document.getElementById('fourPmElInput');
-    if (fourPm < Date.now()) {
+    if (moment().isAfter(fourPm)) {
         fourPmEl.style.backgroundColor = 'red';
         fourPmElInput.style.backgroundColor = 'red';
         // added display none below so that once time passes, you can no longer input a task
@@ -143,7 +144,7 @@ function updateBackground() {
     //5pm section
     const fivePmEl = document.getElementById('fivePmEl');
     const fivePmElInput = document.getElementById('fivePmElInput');
-    if (fivePm < Date.now()) {
+    if (moment().isAfter(fivePm)) {
         fivePmEl.style.backgroundColor = 'red';
         fivePmElInput.style.backgroundColor = 'red';
         // added display none below so that once time passes, you can no longer input a task
@@ -153,16 +154,46 @@ function updateBackground() {
         fivePmElInput.style.backgroundColor = 'green';
     };
 
+    // 6pm section
+    const sixPmEl = document.getElementById('sixPmEl');
+    const sixPmElInput = document.getElementById('sixPmElInput');
+    if (moment().isAfter(sixPm)) {
+        sixPmEl.style.backgroundColor = 'red';
+        sixPmElInput.style.backgroundColor = 'red';
+        // added display none below so that once time passes, you can no longer input a task
+        sixPmElInput.style.display = 'none';
+    } else {
+        sixPmEl.style.backgroundColor = 'green';
+        sixPmElInput.style.backgroundColor = 'green';
+    };
+
+
 };
 
+allNames = []
 
-var saveButton = document.querySelector('#save-tasks');
+function submit() {
+    var input8 = document.getElementById("sixPmElInput").value;
+    var allNames = JSON.parse(localStorage.getItem("allNames")) || [];
+    allNames.push(input8);
+    localStorage.setItem("allNames", JSON.stringify(allNames));
+    document.getElementById("sixPmElInput").value = '';
+    document.getElementById("sixPmEl").textContent = localStorage.getItem("allNames");
+}
 
-saveButton.addEventListener('click', function () {
+document.getElementById("sixPmElInput").textContent = localStorage.getItem("allNames");
 
-    var taskInput = document.querySelector('#eightAmElInput', '#nineAmElInput', '#tenAmElInput', '#elevenAmElInput', '#twelvePmElInput', '#onePmElInput', '#twoPmElInput', '#threePmElInput', '#fourPmElInput', '#fivePmElInput');
-    localStorage.setItem('inputName', taskInput.val());
-})
+setInterval(checkTime, 1000 * 60);
+checkTime();
 
-setInterval(updateBackground, 1000 * 60);
-updateBackground();
+// '#eightAmElInput', '#nineAmElInput', '#tenAmElInput', '#elevenAmElInput', '#twelvePmElInput', '#onePmElInput', '#twoPmElInput', '#threePmElInput', '#fourPmElInput', '#fivePmElInput'
+
+// const nineAm = new Date('June 7, 2022 09:00:00');
+// const tenAm = new Date('June 7, 2022 10:00:00');
+// const elevenAm = new Date('June 7, 2022 11:00:00');
+// const twelvePm = new Date('June 7, 2022 12:00:00');
+// const onePm = new Date('June 7, 2022 13:00:00');
+// const twoPm = new Date('June 7, 2022 14:00:00');
+// const threePm = new Date('June 7, 2022 15:00:00');
+// const fourPm = new Date('June 7, 2022 16:00:00');
+// const fivePm = new Date('June 7, 2022 17:00:00');
